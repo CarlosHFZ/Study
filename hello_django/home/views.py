f160ab50b4c789784919c95da9e8e963252a24fe
Carlos Henrique <kalizehnder@outlook.com>
2025-03-21 16:31:35 -0300

    Django studies

--- hello_django/home/views.py
+++ hello_django/home/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    print('home')
+    print('HOME')
+    # RENDER ---> busca arquivos dos diretórios: TEMPLATES e BASE
     return render(
         request,
-        'home/index.html'
+        'home/index.html',  # Busca automaticamente todos os arquivos que estão dentro da pasta TEMPLATES na raiz da pasta do APP
     )
